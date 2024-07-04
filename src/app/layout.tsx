a399d80ee340c6b39e910e9e64f3bd3b34dcd30d
Pedro Santana <pedrosantana@duck.com>
2024-07-04 12:02:23 +0100

    Enhance main Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,32 @@ import { Toaster } from "@/components/ui/sonner";
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { TRPCReactProvider } from "@/trpc/react";
+import type { Metadata } from "next";
 
 import "@/styles/globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Farlonger",
   description:
     "A sufficiently decentralized blogging platform built on Farcaster and IPFS.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://farlonger.xyz",
+    title: "Farlonger",
+    description:
+      "A sufficiently decentralized blogging platform built on Farcaster and IPFS.",
+    siteName: "Farlonger",
+    images: [
+      {
+        url: "https://farlonger.com/og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Farlonger",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
