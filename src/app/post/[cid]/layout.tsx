f7e2c40d213e68789749e022f48e4c86bfa05292
Pedro Santana <pedrosantana@duck.com>
2024-07-08 23:05:55 +0100

    Responsive design

--- src/app/post/[cid]/layout.tsx
+++ src/app/post/[cid]/layout.tsx
@@ -13,7 +13,7 @@ export default function PostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex w-full flex-col items-center justify-start">
+    <div className="flexs w-full flex-col items-center justify-start">
       {children}
     </div>
   );
