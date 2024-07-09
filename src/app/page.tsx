b850c2580c21d149648fdc142afb3480631f7631
Pedro Santana <pedrosantana@duck.com>
2024-07-09 08:06:48 +0100

    UI tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const { data: forYou, isLoading } = api.post.getLatest.useQuery();
 
   return (
-    <main className="flex min-h-[calc(100vh-9rem-1px)] w-full flex-col items-start justify-between p-4 pt-28 md:flex-row md:px-12 lg:px-24">
+    <main className="flex min-h-[calc(100vh-9rem-1px)] w-full flex-col items-start justify-between gap-4 p-4 pt-28 md:flex-row md:px-12 lg:px-24">
       <div className="flex w-full flex-col items-center justify-center gap-4 md:w-[calc(100%-200px)]">
         <Feed forYou={forYou ?? []} loading={isLoading ?? false} />
       </div>
