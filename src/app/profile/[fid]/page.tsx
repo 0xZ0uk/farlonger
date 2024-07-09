73767572d910307d134be11f5397310403549dda
Pedro Santana <pedrosantana@duck.com>
2024-07-09 08:04:30 +0100

    Skeleton UI

--- src/app/profile/[fid]/page.tsx
+++ src/app/profile/[fid]/page.tsx
@@ -2,6 +2,7 @@
 
 import { Posts } from "@/app/_components/posts";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { fetcher } from "@/lib/swr";
 import { api } from "@/trpc/react";
 import Image from "next/image";
@@ -10,16 +11,14 @@ import { useEffect } from "react";
 import useSWR from "swr";
 
 export default function ProfileByFID({ params }: { params: { fid: string } }) {
-  const { data, error, isLoading } = useSWR(
-    `/api/hub/following?fid=${params.fid}`,
-    fetcher,
-  );
+  const { data } = useSWR(`/api/hub/following?fid=${params.fid}`, fetcher);
 
-  const { data: user } = api.user.getUserByFID.useQuery({
-    fid: Number(params.fid) ?? 1,
-  });
+  const { data: user, isLoading: isUserLoading } =
+    api.user.getUserByFID.useQuery({
+      fid: Number(params.fid) ?? 1,
+    });
 
-  const { data: posts } = api.post.getByUserFID.useQuery({
+  const { data: posts, isLoading } = api.post.getByUserFID.useQuery({
     fid: params.fid ?? 1,
   });
 
@@ -30,11 +29,13 @@ export default function ProfileByFID({ params }: { params: { fid: string } }) {
   return (
     <main className="flex min-h-[calc(100vh-9rem-1px)] items-start justify-between p-8 pt-28 md:px-12 lg:px-24">
       <div className="absolute left-8 flex w-[calc(100%-4rem)] flex-col items-center justify-center gap-4 sm:left-12 md:w-[calc(100%-24rem-7rem)] lg:left-24 lg:w-[calc(100%-24rem-13rem)]">
-        <div className="w-full">
-          <h1 className="mb-8 text-6xl font-bold">
-            {user?.username ?? "Display Name"}
-          </h1>
-          <Posts posts={posts ?? []} />
+        <div className="mt-12 w-full">
+          {!!user ? (
+            <h1 className="mb-8 text-6xl font-bold">{user?.username}</h1>
+          ) : (
+            <Skeleton className="mb-8 h-20 w-64 font-bold" />
+          )}
+          <Posts posts={posts ?? []} loading={isLoading ?? false} />
         </div>
       </div>
       <div className="fixed right-8 hidden min-h-[calc(100vh-9rem-1px)] flex-col gap-4 border-l  border-muted pl-4 sm:right-12 sm:hidden sm:w-0 md:flex md:w-96 lg:right-24">
